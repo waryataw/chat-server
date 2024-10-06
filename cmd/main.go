72c8d133692cdcc448bfa00b776894435f452c4a
Anton Waryat <waryataw@gmail.com>
2024-10-06 11:10:41 +0300

    chat api

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,50 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	"github.com/brianvoe/gofakeit/v7"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	desc "github.com/waryataw/chat-server/pkg/chat_v1"
 )
 
+const grpcPort = 50052
+
+type server struct {
+	desc.UnimplementedChatV1Server
+}
+
+func (s *server) Create(context.Context, *desc.CreateRequest) (*desc.CreateResponse, error) {
+	return &desc.CreateResponse{Id: gofakeit.Int64()}, nil
+}
+
+func (s *server) Delete(context.Context, *desc.DeleteRequest) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) SendMessage(context.Context, *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,16 @@
-module github.com/waryataw/microservices_course_boilerplate
+module github.com/waryataw/chat-server
 
 go 1.23
 
-require github.com/fatih/color v1.17.0
+require (
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
+)
 
 require (
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/brianvoe/gofakeit/v7 v7.0.4 // indirect
+	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
 )
