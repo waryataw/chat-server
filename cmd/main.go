36e42241d0a3a870b8e3d91fcfc8db78cbace16e
Anton Waryat <waryataw@gmail.com>
2024-10-12 18:50:31 +0300

    all grpc methods implementation

--- cmd/main.go
+++ cmd/main.go
@@ -10,15 +10,18 @@ import (
 	"github.com/waryataw/chat-server/internal/config"
 	"google.golang.org/grpc/credentials/insecure"
 
-	"github.com/brianvoe/gofakeit/v7"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
 
+	sq "github.com/Masterminds/squirrel"
+
 	descUser "github.com/waryataw/auth/pkg/user_v1"
 	desc "github.com/waryataw/chat-server/pkg/chat_v1"
 )
+
+const errNoRows = "no rows in result set"
 
 var configPath string
 
@@ -32,24 +35,144 @@ type server struct {
 	grpcClient descUser.UserV1Client
 }
 
-func (s *server) Create(ctx context.Context, _ *desc.CreateRequest) (*desc.CreateResponse, error) {
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+
+	var userIDs []int64
+
+	for _, username := range req.GetUsernames() {
+
+		user, err := s.grpcClient.GetByName(ctx, &descUser.GetByNameRequest{
+			Name: username,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		userIDs = append(userIDs, user.GetUser().GetId())
+
+	}
+
+	builderInsert := sq.Insert("chat").
+		PlaceholderFormat(sq.Dollar).
+		Columns("created_at").
+		Values(sq.Expr("NOW()")).
+		Suffix("RETURNING id")
+
+	query, args, err := builderInsert.ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build query to insert chat: %v", err)
+	}
+
+	var chatID int64
+	err = s.pool.QueryRow(ctx, query, args...).Scan(&chatID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert chat: %v", err)
+	}
+
+	log.Printf("inserted chat with id: %d", chatID)
+
+	for _, id := range userIDs {
+
+		builderInsertChatUser := sq.Insert("chat_user").
+			PlaceholderFormat(sq.Dollar).
+			Columns("chat_id", "user_id").
+			Values(chatID, id)
+
+		query, args, err = builderInsertChatUser.ToSql()
+		if err != nil {
+			return nil, fmt.Errorf("failed to build query to insert chat_user: %v", err)
+		}
+
+		_, err = s.pool.Exec(ctx, query, args...)
+		if err != nil {
+			return nil, fmt.Errorf("failed to insert chat_user: %v", err)
+		}
+
+	}
+
+	return &desc.CreateResponse{Id: chatID}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+
+	bqs := sq.Select("1").
+		From("chat").
+		PlaceholderFormat(sq.Dollar).
+		Where(sq.Eq{"id": req.GetId()}).
+		Limit(1)
+
+	query, args, err := bqs.ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build query: %v", err)
+	}
+
+	var exist int
+
+	err = s.pool.QueryRow(ctx, query, args...).Scan(&exist)
+	if err != nil {
+		if err.Error() == errNoRows {
+			return nil, fmt.Errorf("chat: %d not founded", req.GetId())
+		}
+		return nil, fmt.Errorf("failed to execute query: %v", err)
+	}
+
+	bqd := sq.Delete("chat").PlaceholderFormat(sq.Dollar).Where(sq.Eq{"id": req.GetId()})
+
+	query, args, err = bqd.ToSql()
+	_, err = s.pool.Exec(ctx, query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute query: %v", err)
+	}
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
+
 	user, err := s.grpcClient.GetByName(ctx, &descUser.GetByNameRequest{
-		Name: "magna",
+		Name: req.GetFrom(),
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Println(user)
+	// Пока выберу первый попавщийся, потом будет совсем иначе все
+	bQSelectChat := sq.Select("chat_id").
+		From("chat_user").
+		PlaceholderFormat(sq.Dollar).
+		Where(sq.Eq{"user_id": user.GetUser().GetId()}).
+		Limit(1)
 
-	return &desc.CreateResponse{Id: gofakeit.Int64()}, nil
-}
+	query, args, err := bQSelectChat.ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build query: %v", err)
+	}
+	var chatID int64
 
-func (s *server) Delete(context.Context, *desc.DeleteRequest) (*emptypb.Empty, error) {
-	return &emptypb.Empty{}, nil
-}
+	// Выполняем запрос и сканируем результат
+	err = s.pool.QueryRow(ctx, query, args...).Scan(&chatID)
+	if err != nil {
+		if err.Error() == errNoRows {
+			return nil, fmt.Errorf("no chat found for user: %v", user.GetUser().GetId())
+		}
+		return nil, fmt.Errorf("failed to select chat id: %v", err)
+	}
 
-func (s *server) SendMessage(context.Context, *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	bQInsertMessage := sq.Insert("message").
+		PlaceholderFormat(sq.Dollar).
+		Columns("chat_id", "user_id", "text").
+		Values(chatID, user.User.GetId(), req.GetText())
+
+	query, args, err = bQInsertMessage.ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build query to insert message: %v", err)
+	}
+
+	_, err = s.pool.Exec(ctx, query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert message: %v", err)
+	}
+
 	return &emptypb.Empty{}, nil
 }
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/waryataw/chat-server
 go 1.23
 
 require (
+	github.com/Masterminds/squirrel v1.5.4
 	github.com/brianvoe/gofakeit/v7 v7.0.4
 	github.com/jackc/pgx/v4 v4.18.3
 	github.com/joho/godotenv v1.5.1
@@ -21,6 +22,8 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
