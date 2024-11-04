d09d86d303d76a387e766b5bcd9291104d069e89
Anton Waryat <waryataw@gmail.com>
2024-11-04 12:08:46 +0300

    send message user from cache

--- internal/service/chat/send_message.go
+++ internal/service/chat/send_message.go
@@ -8,9 +8,21 @@ import (
 )
 
 func (s chatService) SendMessage(ctx context.Context, from string, text string) error {
-	user, err := s.authRepository.GetUser(ctx, from)
+	user, err := s.AuthCacheRepository.GetUser(ctx, from)
 	if err != nil {
-		return fmt.Errorf("failed to get user: %w", err)
+		return fmt.Errorf("failed to get user from auth cache: %w", err)
+	}
+
+	if user == nil {
+		user, err = s.authRepository.GetUser(ctx, from)
+		if err != nil {
+			return fmt.Errorf("failed to get user from auth service: %w", err)
+		}
+
+		err = s.AuthCacheRepository.CreateUser(ctx, user)
+		if err != nil {
+			return fmt.Errorf("failed to create user in cache: %w", err)
+		}
 	}
 
 	chat, err := s.repository.Get(ctx, user)
